c58a316ff749f3da9c1d5c2e1278b3c12caf0b2c
Marisela Ruiz <115127240+marisela-ruiz@users.noreply.github.com>
2023-01-15 13:55:32 -0600

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,11 @@
+const balls = document.getElementsByClassName('ball');
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+};
